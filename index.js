a7211e386e6858152d1ea1cfa90000c05f61975a
Md Rijoan Maruf <rmsaif4430@gmail.com>
2025-07-24 02:20:05 +0600

    Basic express setup and mongodb

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 
 app.use(cors());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0ykpaho.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    const db = client.db('eCommerce');
+    const userCollection = db.collection('users')
+
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+  }
+}
+run().catch(console.dir);
+
 
 // Basic Route
 app.get('/', (req, res) => {
